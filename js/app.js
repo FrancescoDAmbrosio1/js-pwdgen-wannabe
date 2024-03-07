d46fbde7a56ea7f1de29756da25a438434d7c6f2
FrancescoDAmbrosio1 <ciccio.dambrosio@hotmail.it>
2024-03-07 15:16:27 +0100

    Modify: random number in  const userPwd

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const userName = prompt('Come ti chiami?'); //string
 const userSurname = prompt('Quale è il tuo cognome?'); //string
 const userColor = prompt('Quale è il tuo colore freferito?'); //string
-const userPwd = userName + userSurname + userColor + 21; //string
-console.log('La tua password è: ',userPwd)
+const randomNumber = Math.round(Math.random()*100) //number
+const userPwd = userName + userSurname + userColor + randomNumber; //string
+console.log('La tua password è: ',userPwd); //string
