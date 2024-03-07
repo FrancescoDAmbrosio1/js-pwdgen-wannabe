366781b6b8664f145ff211c0dbdb0e6b821421f3
FrancescoDAmbrosio1 <ciccio.dambrosio@hotmail.it>
2024-03-07 14:58:13 +0100

    ADD: app.js with const userPwd

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const userName = prompt('Come ti chiami?'); //string
+const userSurname = prompt('Quale è il tuo cognome?'); //string
+const userColor = prompt('Quale è il tuo colore freferito?'); //string
+const userPwd = userName + userSurname + userColor + 21; //string
+console.log('La tua password è: ',userPwd)
